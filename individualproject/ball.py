74d64ccb1c3d2057b2ee4bff80e7f6bef5a15b24
mor19-meet <mor19@meet.mit.edu>
2018-01-10 19:32:47 +0200

    updated

--- individualproject/ball.py
+++ individualproject/ball.py
@@ -1,31 +1,50 @@
-import turtle
+from turtle import *
 
-turtle.shape('circle')
-turtle.tracer(1,0)
+# turtle.shape('circle')
+# turtle.tracer(1,0)
 
-class Ball(object):
-	def__init__(self,x,y,shape,dx,dy,r,color)
-		self.x=x
-		self.y=y
-		self.dx=dx
-		self.dy=dy
-		self.r=r
-		self.color=color
-a= Ball(0,0,'circle',100, 500, r/10,'blue')
+class Ball(Turtle):
+    def __init__(self,x,y,dx,dy,r,color):
+        Turtle.__init__(self)
+        self.penup()
+        self.x=x
+        self.y=y
+        self.dx=dx
+        self.dy=dy
+        self.r=r
+        self.color(color)
+        self.shape('circle')
 
-turtle.penup()
+    def move(self, screen_width, screen_height):
+        self.screen_width=screen_width
+        self.screen_height=screen_height
+        current_x= self.xcor()
+        new_x= current_x + self.dx
+        current_y= self.ycor()
+        new_y= current_y + self.dy
 
-class move(object):
-	def__init__(self, screen_width, screen_height)
-		self.screen_width=screen_width
-		self.screen_height=screen_height
-	current_x=[0]
-	new_x= self.current_x + self.move(dx)
-	current_y=[0]
-	new_y= self.current_y + self.move(dy)
-	right_side_ball= new_x + self.move(r)
-	left_side_ball= new_x - self.move(r)
-	top_side_ball= new_y + self.move(r)
-	bottom_side_ball= new_y - self.move(r)
-self.goto(new_x, new_y)
-turtle.mainloop()
+        self.goto(new_x, new_y)
+                
+        right_side_ball= new_x + self.r
+        left_side_ball= new_x - self.r
+        top_side_ball= new_y + self.r
+        bottom_side_ball= new_y - self.r
+
+        if top_side_ball>= screen_height/2:
+            self.goto(self.dx,-self.dy)
+        if bottom_side_ball>= -screen_height/2:
+                self.goto(self.dx, self.dy)
+        if left_side_ball>= -screen_width/2:
+                self.goto(self.dx, -self.dy)
+        if right_side_ball>= screen_width/2:
+                self.goto(self.dx,self.dy)
+        
+
+
+a= Ball(0,0,100, 500, 10/10,'blue')
+flag = True
+while flag:
+    a.move(500,500)
+
+# self.goto(new_x, new_y)
+
