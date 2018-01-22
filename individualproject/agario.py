442cf129fe7af3071dd2c415d287770dbefcf930
mor19-meet <mor19@meet.mit.edu>
2018-01-22 19:16:00 +0200

    22/01/2018

--- individualproject/agario.py
+++ individualproject/agario.py
@@ -23,28 +23,51 @@ MAXIMUM_BALL_DY= 5
 BALLS= []
 
 for i in range(NUMBER_OF_BALLS):
-	dx=random.randint(-SCREEN_WIDTH + MAXIMUM_BALL_RADIUS, -SCREEN_HEIGHT + MAXIMUM_BALL_RADIUS)
-	while(dx==0):
-		dx=random.randint(-SCREEN_WIDTH + MAXIMUM_BALL_RADIUS, -SCREEN_HEIGHT + MAXIMUM_BALL_RADIUS)
-	dy=random.randint(SCREEN_WIDTH - MAXIMUM_BALL_RADIUS, SCREEN_HEIGHT - MAXIMUM_BALL_RADIUS)
-	while(dy==0):
-		dy=random.randint(SCREEN_WIDTH - MAXIMUM_BALL_RADIUS, SCREEN_HEIGHT - MAXIMUM_BALL_RADIUS)	
-	MINIMUM_BALL_DX<dx<MAXIMUM_BALL_DX
-	MINIMUM_BALL_DY<dy<MAXIMUM_BALL_DY
-	MINIMUM_BALL_RADIUS<r<MAXIMUM_BALL_RADIUS
-	'color'=random.random()
-def new_ball():
-	BALLS.append(new_balls)
-
-for move_all_balls in BALLS():
-
-
-
-
-
-
-
-
-
-
 	
+	x=random.randint(-SCREEN_WIDTH + MAXIMUM_BALL_RADIUS, -SCREEN_HEIGHT + MAXIMUM_BALL_RADIUS)
+	
+	dx=random.randint(MINIMUM_BALL_DX , MAXIMUM_BALL_DX)
+	while(dx==0):
+		x=random.randint(-SCREEN_WIDTH + MAXIMUM_BALL_RADIUS, -SCREEN_HEIGHT + MAXIMUM_BALL_RADIUS)
+	
+	y=random.randint(SCREEN_WIDTH - MAXIMUM_BALL_RADIUS, SCREEN_HEIGHT - MAXIMUM_BALL_RADIUS)
+	
+	dy=random.randint(MINIMUM_BALL_DY , MAXIMUM_BALL_DY)
+	while(dy==0):
+		y=random.randint(SCREEN_WIDTH - MAXIMUM_BALL_RADIUS, SCREEN_HEIGHT - MAXIMUM_BALL_RADIUS)	
+	
+	r=random.randint(MINIMUM_BALL_RADIUS , MAXIMUM_BALL_RADIUS) 
+	
+	color=random.random()
+	Ball(0,0,100, 500, 10/10,'blue')
+
+BALLS.append(Ball)
+
+for move_all_balls in BALLS(screen_width, screen_height):
+	move(500,500)
+
+def collide(ball_a, ball_b):
+	if ball_a==ball_b:
+		return False
+	calculate.distance(ball_a.r , ball_b.r):
+		distance= math.sqrt(math.pow((ball_a.r.MAXIMUM_BALL_RADIUS - ball_a.r.MINIMUM_BALL_RADIUS),2) + math.pow((ball_b.r.MAXIMUM_BALL_RADIUS - ball_b.r.MINIMUM_BALL_RADIUS),2))	
+	if Ddistance+10 <= (ball_a.r+ball_b.r):
+		return True
+	else:
+		return False
+
+def check_all_balls_collision():
+	for ball_a in BALLS:
+	for ball_b in BALLS:
+	if collide(ball_a, ball_b)==True:
+		a_radius = ball_a.r()
+		b_radius = ball_b.r()
+	X_coordinate=random.randint(round(-SCREEN_WIDTH), round(SCREEN_WIDTH))
+	Y_coordinate=random.randint(round(-SCREEN_HEIGHT, round(SCREEN_HEIGHT))
+	dx=random.randint(MINIMUM_BALL_DX, MAXIMUM_BALL_DX)
+	dy=random.randint(MINIMUM_BALL_DY, MAXIMUM_BALL_DY)
+	r=random.randint(MINIMUM_BALL_RADIUS, MAXIMUM_BALL_RADIUS)
+	
+
+
+
